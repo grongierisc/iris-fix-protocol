816706b47ee9552a8bf0b9e85b04575438b67407
Enard Lucas <lucas.enard@hotmail.com>
2022-07-27 10:15:52 +0200

    Update bp.py

--- src/fix/bp.py
+++ src/fix/bp.py
@@ -1,5 +1,6 @@
 from grongier.pex import BusinessProcess
 from msg import Request,MarketResponse
+from obj import MessageField,HeaderField,GroupField
 
 class FixBusinessProcess(BusinessProcess):
 
@@ -14,16 +15,19 @@ class FixBusinessProcess(BusinessProcess):
         try:
             msg_type = getattr(request.header_field,"35")
 
-            if  msg_type == "D":
-                quote_req = Request()
-                setattr(quote_req.message_field,"55",getattr(request,"55"))
-                setattr(quote_req.message_field,"13","2")
-                setattr(quote_req.message_field,"40",getattr(request,"40"))
+            if msg_type == "D":
+                msg_field = MessageField()
+
+                setattr(msg_field,"55",getattr(request.message_field,"55"))
+                setattr(msg_field,"13","2")
+                setattr(msg_field,"40",getattr(request.message_field,"40"))
+
+                quote_req = Request(msg_field)
             
                 quote_resp = self.send_request_sync("Python.FixQuote",quote_req)
 
                 if quote_resp.msg != "Time Out 5s":
-                    setattr(request,"117",getattr(quote_resp,"117"))
+                    setattr(request.message_field,"117",getattr(quote_resp,"117"))
                     order_resp = self.send_request_sync("Python.FixOrder",request)
                     return order_resp
                 else:
